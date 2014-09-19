7f4b0feab6231e99c3163ceb115c17a23a27a5e1
Patrick Schoeps <schoeps.pat@gmail.com>
2014-09-19 12:33:11 -0400

    working

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,5 @@
 
 ActiveRecord::Schema.define(version: 20140916232819) do
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
+  
 end
